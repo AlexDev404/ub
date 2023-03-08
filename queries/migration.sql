36ce4acd63bf7d446daa4a2be0d962fca5452ca4
AlexDev404 <alexdev404@users.noreply.github.com>
2023-03-07 23:52:10 -0600

    Finish database migrations. Everything is working as expected

--- queries/migration.sql
+++ queries/migration.sql
@@ -42,7 +42,6 @@ CREATE TABLE Feeder (
     feeder text NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
-
 --- Students ---
 CREATE TABLE Students (
     student_id int PRIMARY KEY,
@@ -59,67 +58,32 @@ CREATE TABLE Students (
     program_status_id int REFERENCES Program_Status(program_status_id) NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
-
 --- Courses ---
 CREATE TABLE Courses (
     courses_id serial PRIMARY KEY,
     student_id int REFERENCES Students(student_id),
     enrolled_semester text NOT NULL,
     course_id int REFERENCES Course(course_id) NOT NULL,
-    course_grade text NOT NULL,
+    course_grade text NOT NULL DEFAULT 'IN_PROGRESS',
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
 
 --- Start Imports (Start from Top-to-Bottom) ---
-
---- Students ---
-\COPY Students(
-    student_id,
-    gender,
-    ethnicity_id,
-    district_id,
-    feeder_id,
-    program_status_id,
-    program_start,
-    program_end,
-    grad_date
-) FROM '../data/Students.csv' DELIMITER ',' CSV HEADER;
-
---- Courses ---
-\COPY Courses(
-    courses_id,
-    student_id,
-    enrolled_semester,
-    course_id,
-    course_grade
-) FROM '../data/Courses.csv' DELIMITER ',' CSV HEADER;
-
+--- Commands must be single-lined
+--- Imported in dependent sequence ---
+--- DO NOT CHANGE!!!! ---
 --- Course ---
-\COPY Course(
-    course_id,
-    course_code,
-    course_title,
-    course_credits
-) FROM '../data/Course.csv' DELIMITER ',' CSV HEADER;
-
+\COPY Course( course_id, course_code, course_title, course_credits ) FROM '../data/Course.csv' DELIMITER ',' CSV HEADER;
 --- Ethnicity ---
-\COPY Ethnicity(ethnicity_id, ethnicity)
-FROM '../data/Ethnicity.csv' DELIMITER ',' CSV HEADER;
-
+\COPY Ethnicity(ethnicity_id, ethnicity) FROM '../data/Ethnicity.csv' DELIMITER ',' CSV HEADER;
 --- Program_Status ---
-\COPY Program_Status(
-    program_status_id,
-    program_status
-) FROM '../data/Program_Status.csv' DELIMETER ',' CSV HEADER;
-
+\COPY Program_Status( program_status_id, program_status ) FROM '../data/Program_Status.csv' DELIMITER ',' CSV HEADER;
 --- District ---
-\COPY District(district_id, district)
-FROM '../data/District.csv' DELIMITER ',' CSV HEADER;
-
+\COPY District(district_id, district) FROM '../data/District.csv' DELIMITER ',' CSV HEADER;
 --- Feeder ---
-\COPY Feeder(
-    feeder_id,
-    feeder
-) FROM '../data/Feeder.csv' DELIMETER ',' CSV HEADER;
-
+\COPY Feeder( feeder_id, feeder ) FROM '../data/Feeder.csv' DELIMITER ',' CSV HEADER;
+--- Students ---
+\COPY Students( student_id, gender, ethnicity_id, district_id, feeder_id, program_status_id, program_start, program_end, grad_date ) FROM '../data/Students.csv' DELIMITER ',' CSV HEADER; 
+--- Courses ---
+\COPY Courses( courses_id, student_id, enrolled_semester, course_id, course_grade ) FROM '../data/Courses.csv' DELIMITER ',' CSV HEADER;
 --- FINISH ---
